7e818e11092994605a3f2c8d74e1c7c8b19b51ea
gsBullet <glmsorowar@gmail.com>
2025-09-21 19:32:25 +0600

    edit fil

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.send('Hello, Express! from AWS EC2');
 });
 
 app.listen(PORT, () => {
